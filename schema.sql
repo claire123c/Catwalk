5b24044c4aa308523d81ca4c4b7ad6f409ae24fa
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-23 15:50:29 -0700

    Edited and inserting data into schema

--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE products;
 
 \c products;
 
-DROP TABLE IF EXISTS
-  "products", "styles", "photos", "skus", "features", "related";
+-- DROP TABLE IF EXISTS
+--   "products", "styles", "photos", "skus", "features", "related";
 
 CREATE TABLE "products" (
   "id" serial PRIMARY KEY,
-  "name" varchar(20) NOT NULL,
-  "slogan" varchar(50) NOT NULL,
+  "name" varchar(100) NOT NULL,
+  "slogan" varchar(200) NOT NULL,
   "description" varchar(1000) NOT NULL,
   "category" varchar(20) NOT NULL,
   "default_price" varchar(10) NOT NULL
@@ -18,47 +18,60 @@ CREATE TABLE "products" (
 
 CREATE TABLE "styles" (
   "id" serial PRIMARY KEY,
-  "product_id" varchar(40),
-  "original_price" varchar(10),
+  "product_id" int,
+  "name" varchar(100) NOT NULL,
   "sale_price" varchar(10),
+  "original_price" varchar(10),
   "default?" boolean DEFAULT false
 );
 
 CREATE TABLE "photos" (
   "id" serial PRIMARY KEY,
   "styles_id" int,
-  "thumbnail_url" varchar(2083) NOT NULL,
-  "url" varchar(2083) NOT NULL
+  "url" text NOT NULL,
+  "thumbnail_url" text NOT NULL
 );
 
 CREATE TABLE "skus" (
   "id" serial PRIMARY KEY,
-  "quantity" int,
   "styles_id" int,
-  "size" varchar(5)
+  "size" varchar(20),
+  "quantity" int
 );
 
 CREATE TABLE "features" (
   "id" serial PRIMARY KEY,
   "product_id" int,
-  "feature" varchar(20),
+  "feature" varchar(50),
   "value" varchar(50)
 );
 
 CREATE TABLE "related" (
-  "related_id" serial PRIMARY KEY,
-  "product1_id" int,
-  "product2_id" int
+  "id" serial PRIMARY KEY,
+  "current_product_id" int,
+  "related_product_id" int
 );
 
-ALTER TABLE "related" ADD FOREIGN KEY ("product1_id") REFERENCES "products" ("id");
+ALTER TABLE "related" ADD FOREIGN KEY ("current_product_id") REFERENCES "products" ("id");
 
-ALTER TABLE "related" ADD FOREIGN KEY ("product2_id") REFERENCES "products" ("id");
+ALTER TABLE "related" ADD FOREIGN KEY ("related_product_id") REFERENCES "products" ("id");
 
 ALTER TABLE "features" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 
-ALTER TABLE "styles" ADD FOREIGN KEY ("id") REFERENCES "products" ("id");
+ALTER TABLE "styles" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 
-ALTER TABLE "styles" ADD FOREIGN KEY ("id") REFERENCES "skus" ("id");
+ALTER TABLE "photos" ADD FOREIGN KEY ("styles_id") REFERENCES "styles" ("id");
 
-ALTER TABLE "styles" ADD FOREIGN KEY ("id") REFERENCES "photos" ("id");
+ALTER TABLE "skus" ADD FOREIGN KEY ("styles_id") REFERENCES "styles" ("id");
+
+COPY products FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/product.csv' WITH (FORMAT csv, HEADER);
+
+-- COPY styles FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/styles.csv' WITH (FORMAT csv, HEADER);
+
+-- COPY photos FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/photos.csv' WITH (FORMAT csv, HEADER);
+
+-- COPY skus FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/skus.csv' WITH (FORMAT csv, HEADER);
+
+-- COPY features FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/features.csv' WITH (FORMAT csv, HEADER);
+
+COPY related FROM '/Users/yawnsandsmiles/SFO135/SDC/Products/data/related.csv' WITH (FORMAT csv, HEADER);
