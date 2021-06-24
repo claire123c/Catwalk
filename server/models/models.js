0dc2613adbcdcc4206e6b80884d210ea734079dd
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-24 14:06:34 -0700

    queried product data

--- server/models/models.js
+++ server/models/models.js
@@ -2,7 +2,7 @@ const pool = require('../../db/index.js');
 
 module.exports = {
   getAllProducts: (page = 1, count = 5, callback) => {
-    const query = 'SELECT * from products LIMIT $1'
+    const query = 'SELECT * FROM products LIMIT $1'
     pool.query(query, [count], (err, data) => {
       if (err) {
         callback(err);
@@ -13,9 +13,13 @@ module.exports = {
   },
 
   getOneProduct: (id, callback) => {
-    const query = 'SELECT * from products WHERE id = $1'
+    //WHERE products.id = $1'
+    // const query = 'SELECT * FROM products INNER JOIN features ON products.id = features.product_id WHERE products.id = $1'
+    // SELECT * FROM products WHERE products.id = $1 UNION SELECT feature, value FROM features WHERE product_id = $1
+    const query = 'SELECT * FROM features INNER JOIN products ON products.id = features.product_id WHERE products.id = $1'
     pool.query(query, [id], (err, data) => {
       if (err) {
+        console.log(err);
         callback(err);
       } else {
         callback(null, data.rows);
