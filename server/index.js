bb4b3dffebecbf7dacd91f8859240f52eefb8e48
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-23 22:35:52 -0700

    Creating structure for folders

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const db = require('../db/index.js');
 const port = 3000;
+const router = require('./routes.js');
 
 const app = express();
 
+app.use('/', router);
 app.listen(port, () => {
   console.log(`Server listening on port ${3000}`);
 });
