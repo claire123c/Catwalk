76831744f78136f2d3d7e352a620805921e17843
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-25 22:15:11 -0700

    Added photos array

--- server/models/models.js
+++ server/models/models.js
@@ -48,8 +48,12 @@ module.exports = {
   getStyles: (id, callback) => {
     const query = `
     SELECT styles.product_id,
-      (SELECT json_agg(json_build_object('style_id', styles.id, 'name', styles.name, 'original_price', styles.original_price, 'sale_price', styles.sale_price, 'default?', styles."default?", 'skus', (SELECT json_object_agg(skus.id, json_build_object('quantity', skus.quantity, 'size', skus.size))
-        FROM skus WHERE skus.styles_id = styles.id)))
+      (SELECT json_agg(json_build_object('style_id', styles.id, 'name', styles.name, 'original_price', styles.original_price, 'sale_price', styles.sale_price, 'default?', styles."default?", 'photos',
+        (SELECT json_agg(photos.id)
+        FROM photos WHERE photos.styles_id = styles.id),
+            'skus',
+        (SELECT json_object_agg(skus.id, json_build_object('quantity', skus.quantity, 'size', skus.size))
+          FROM skus WHERE skus.styles_id = styles.id)))
         FROM styles WHERE styles.product_id = $1) AS results
       FROM styles
       WHERE styles.product_id = $1 LIMIT 1;`
