3f1b09d9be898a3eb36d4dd271b0a17c1d24228c
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 12:24:50 -0700

    Added test to see if response is json

--- server-tests/app.test.js
+++ server-tests/app.test.js
@@ -3,10 +3,13 @@ import pool from '../db/index.js';
 import app from '../server/app.js';
 
 describe('GET /products', () => {
-  afterEach(()=> {
+  afterAll(()=> {
     pool.end();
   })
   test('should respond with a 200 status code', async () => {
     await request(app).get('/products').expect(200);
+  })
+  test('response with json', async () => {
+    await request(app).get('/products').expect('Content-Type', /json/);
   })
 })
