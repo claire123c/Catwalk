d128efaa8b905f5246c5534aabedfc758268dfa3
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 12:44:47 -0700

    Added get test for one product and limited count

--- server-tests/app.test.js
+++ server-tests/app.test.js
@@ -2,14 +2,24 @@ import request from 'supertest';
 import pool from '../db/index.js';
 import app from '../server/app.js';
 
+afterAll(()=> {
+  pool.end();
+})
+
 describe('GET /products', () => {
-  afterAll(()=> {
-    pool.end();
-  })
   test('should respond with a 200 status code', async () => {
     await request(app).get('/products').expect(200);
   })
   test('response with json', async () => {
     await request(app).get('/products').expect('Content-Type', /json/);
+  })
+})
+
+describe('GET /products/:product_id', () => {
+  test('should respond with a 200 status code', async () => {
+    await request(app).get('/products/7893').expect(200);
+  })
+  test('response with json', async () => {
+    await request(app).get('/products/7893').expect('Content-Type', /json/);
   })
 })
