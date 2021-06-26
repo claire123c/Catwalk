5483b163b34dd46dd7956b3cae27c75eaf09f7e9
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 13:55:35 -0700

    Added tests for 404 and if flatten input is not array

--- server-tests/app.test.js
+++ server-tests/app.test.js
@@ -23,6 +23,9 @@ describe('GET /products/:product_id', () => {
   test('should respond with a 200 status code', async () => {
     await request(app).get('/products/62621').expect(200);
   })
+  test('should respond with a 404 status code with invalid endpoint', async () => {
+    await request(app).get('/products/invalid').expect(404);
+  })
   test('response with json', async () => {
     await request(app).get('/products/62621').expect('Content-Type', /json/);
   })
@@ -31,6 +34,9 @@ describe('GET /products/:product_id', () => {
 describe('GET /products/:product_id/styles', () => {
   test('should respond with a 200 status code', async () => {
     await request(app).get('/products/2021/styles').expect(200);
+  })
+  test('should respond with a 404 status code with invalid endpoint', async () => {
+    await request(app).get('/products/invalid/styles').expect(404);
   })
   test('response with json', async () => {
     await request(app).get('/products/2021/styles').expect('Content-Type', /json/);
@@ -41,14 +47,22 @@ describe('GET /products/:product_id/related', () => {
   test('should respond with a 200 status code', async () => {
     await request(app).get('/products/135/related').expect(200);
   })
+  test('should respond with a 404 status code with invalid endpoint', async () => {
+    await request(app).get('/products/invalid/related').expect(404);
+  })
   test('response with json', async () => {
     await request(app).get('/products/135/related').expect('Content-Type', /json/);
   })
 })
 
 describe('flatten()', () => {
-  test('flatten() should flatten array of objects', () => {
+  test('should flatten array of objects', () => {
     const arrOfObjs = [{'1': 1}, {'2': 2}, {'3': 3}];
     expect(flatten(arrOfObjs)).toMatchObject([1, 2, 3]);
+  })
+  test('should return if input is not array', () => {
+    const arrOfObjs = 'invalid';
+    expect(flatten(arrOfObjs)).toBeUndefined();
+
   })
 })
