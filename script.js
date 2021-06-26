a57b9794d5a81f4cad0a090b229b3752c4572046
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 15:52:44 -0700

    Added random number generator

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export let options = {
+  vus: 1,
+  duration: '10s',
+};
+
 export default function () {
-  http.get('https://test.k6.io');
+
+  const randomId = Math.round((Math.random() * 1000010) + 1);
+  http.get('http://localhost:3002/products');
   sleep(1);
 }
