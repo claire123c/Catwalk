79fc660c2e1dbac6482c15ce280647519179f1b9
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 12:46:47 -0700

    Added test for related and styles

--- server-tests/app.test.js
+++ server-tests/app.test.js
@@ -17,9 +17,27 @@ describe('GET /products', () => {
 
 describe('GET /products/:product_id', () => {
   test('should respond with a 200 status code', async () => {
-    await request(app).get('/products/7893').expect(200);
+    await request(app).get('/products/62621').expect(200);
   })
   test('response with json', async () => {
-    await request(app).get('/products/7893').expect('Content-Type', /json/);
+    await request(app).get('/products/62621').expect('Content-Type', /json/);
+  })
+})
+
+describe('GET /products/:product_id/styles', () => {
+  test('should respond with a 200 status code', async () => {
+    await request(app).get('/products/2021/styles').expect(200);
+  })
+  test('response with json', async () => {
+    await request(app).get('/products/2021/styles').expect('Content-Type', /json/);
+  })
+})
+
+describe('GET /products/:product_id/related', () => {
+  test('should respond with a 200 status code', async () => {
+    await request(app).get('/products/135/related').expect(200);
+  })
+  test('response with json', async () => {
+    await request(app).get('/products/135/related').expect('Content-Type', /json/);
   })
 })
