5a5fb1fa2f8fa96216ef6a52c02305b50028a1e8
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-26 15:59:17 -0700

    Added checks to k6 test

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export let options = {
-  vus: 1,
+  vus: 5,
   duration: '10s',
 };
 
 export default function () {
+  const max = 1000011;
+  const min = 1;
+  const product_id = Math.round((Math.random() * (max - min)) + min);
 
-  const randomId = Math.round((Math.random() * 1000010) + 1);
-  http.get('http://localhost:3002/products');
+  let res = http.get('http://localhost:3002/products');
+  check (res, {
+    'is status 200': (r) => r.status === 200,
+    'is duration < 250ms': (r) => r.timings.duration < 250,
+  })
   sleep(1);
 }
