a0a84a1f1823be0b88cd69a624c9b33f52cb009c
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-03 10:55:57 -0700

    added morgan

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const router = require('./routes.js');
+const morgan = require('morgan')
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use('/products', router);
 
 module.exports = app;
